492c1bc940957b04e6aa929ca6d4856c4fd38730
cyschneck <22159116+cyschneck@users.noreply.github.com>
2023-03-01 19:58:34 -0700

    plot with extrapolated lines between

--- river_centerline_width.py
+++ river_centerline_width.py
@@ -1,6 +1,8 @@
 # Find the center point and width between lat/long points along river bank
 # Modified from R - CMGO code functionality
+import math
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import csv
 
@@ -23,16 +25,39 @@ def convertColumnsToCSV(text_file):
 		write.writerow(header_fields)
 		write.writerows(rows)
 
-def plotRiver(river_df):
+########################################################################
+
+def plotRiver(river_df, latitude_extrapolation, longitude_extrapolation, nearest_neighbors_dict, save_plot_name):
 	# Plot river based on right/left bank coordinates
 	fig = plt.figure(figsize=(12,12), dpi=100)
-	plt.title("River Coordinates (Latitude/Longitude)")
-	plt.scatter(x=river_df['llat'], y=river_df['llon'], s=1, label="Left Bank")
-	plt.scatter(x=river_df['rlat'], y=river_df['rlon'], s=1, label="Right Bank")
+	plt.title("River Coordinates")
+	plt.scatter(x=river_df['llat'], y=river_df['llon'], s=3, c="black")
+	plt.plot(river_df['llat'], river_df['llon'], c="dodgerblue", label="Left Bank")
+	plt.scatter(x=river_df['rlat'], y=river_df['rlon'], s=3, c="black")
+	plt.plot(river_df['rlat'], river_df['rlon'], c="orange", label="Right Bank")
+	plt.plot(latitude_extrapolation, longitude_extrapolation)
+	plt.plot(nearest_neighbors_dict.keys(), nearest_neighbors_dict.values(), c="red")
+	plt.xlabel("Latitude")
+	plt.ylabel("Longitude")
 	plt.legend()
 	plt.show()
+	fig.savefig(save_plot_name)
 
+########################################################################
 if __name__ == "__main__":
 	convertColumnsToCSV("data/river_coords.txt")
 	df = pd.read_csv("data/river_coords.csv")
-	plotRiver(df)
+
+	# Lines between points on graph
+	latitude_points = []
+	longitude_points = []
+	for index, row in df.iterrows():
+		latitude_points.append([row.llat, row.rlat])
+		longitude_points.append([row.llon, row.rlon])
+
+	nearest_neighbors = {}
+	for index, row in df.iterrows():
+		pass
+
+	# Plot river banks
+	plotRiver(df, latitude_points, longitude_points, nearest_neighbors, "data/river_coords.png")
