b559e20af5d50ea8bd2ab85ee17902034f2fe871
cyschneck <22159116+cyschneck@users.noreply.github.com>
2023-03-02 19:42:22 -0700

    sort bank pairs by KNN

--- river_centerline_width.py
+++ river_centerline_width.py
@@ -40,16 +40,31 @@ def plotRiver(river_df,
 			latitude_extrapolation, longitude_extrapolation, 
 			right_bank_nearest_neighbor_x, right_bank_nearest_neighbor_y,
 			left_bank_nearest_neighbor_x, left_bank_nearest_neighbor_y,
+			right_bank_expanded, left_bank_expanded,
 			save_plot_name):
 	# Plot river based on right/left bank coordinates
 	fig = plt.figure(figsize=(12,12), dpi=100)
+	x = []
+	y = []
+	for i in right_bank_expanded: 
+		x.append(i[1])
+		y.append(i[0])
+	plt.scatter(x, y, c="orange", label="Right Bank Extrapolation")
+	x = []
+	y = []
+	for i in left_bank_expanded: 
+		x.append(i[1])
+		y.append(i[0])
+	plt.scatter(x, y, c="dodgerblue", label="Left Bank Extrapolation")
+	
 	plt.title("River Coordinates")
 	plt.scatter(x=river_df['llon'], y=river_df['llat'], s=3, c="black")
-	plt.plot(river_df['llon'], river_df['llat'], c="dodgerblue", label="Left Bank")
+	#plt.plot(river_df['llon'], river_df['llat'], c="dodgerblue", label="Left Bank")
 	plt.scatter(x=river_df['rlon'], y=river_df['rlat'], s=3, c="black")
-	plt.plot(river_df['rlon'], river_df['rlat'], c="orange", label="Right Bank")
-	plt.plot(right_bank_nearest_neighbor_x, right_bank_nearest_neighbor_y, c="red", linewidth=0.1, label="Right Bank Nearest Neighbor")
-	plt.plot(left_bank_nearest_neighbor_x, left_bank_nearest_neighbor_y, c="green", linewidth=0.1, label="Left Bank Nearest Neighbor")
+	#plt.plot(river_df['rlon'], river_df['rlat'], c="orange", label="Right Bank")
+
+	plt.plot(right_bank_nearest_neighbor_x, right_bank_nearest_neighbor_y, c="red", linewidth=0.5, label="Right Bank Nearest Neighbor")
+	plt.plot(left_bank_nearest_neighbor_x, left_bank_nearest_neighbor_y, c="green", linewidth=0.5, label="Left Bank Nearest Neighbor")
 	plt.xlabel("Longitude")
 	plt.ylabel("Latitude")
 	plt.legend()
@@ -58,9 +73,9 @@ def plotRiver(river_df,
 
 ########################################################################
 if __name__ == "__main__":
-	convertColumnsToCSV("data/river_coords.txt")
+	#convertColumnsToCSV("data/river_coords.txt")
 	df = pd.read_csv("data/river_coords.csv")
-	#df = df.head(100)
+	df = df.head(310)
 
 	# Lines between points on graph
 	latitude_points = []
@@ -75,28 +90,57 @@ if __name__ == "__main__":
 		if not math.isnan(row.llat) and not math.isnan(row.llon):
 			left_bank_pairs.append([row.llat, row.llon])
 
-	# Sort pair of lists by longitude (second value)
+	# Sort pair of lists
+	## Sort in order by KNN
+	def euclidian_distance(a,b):
+		return np.linalg.norm(a-b)
 	right_bank_pairs.sort(key=lambda x: x[1])
+	right_bank_pairs = np.array(right_bank_pairs)
+	right_bank_pairs = sorted(right_bank_pairs, key = lambda point: euclidian_distance(point,right_bank_pairs[0]))
+	right_bank_pairs = [list(x) for x in right_bank_pairs]
 	left_bank_pairs.sort(key=lambda x: x[1])
+	left_bank_pairs = np.array(left_bank_pairs)
+	left_bank_pairs = list(sorted(left_bank_pairs, key = lambda point: euclidian_distance(point,left_bank_pairs[0])))
+	left_bank_pairs = [list(x) for x in left_bank_pairs]
+
+	# Add points between existing points to increase closest neighbors
+	def expand_list(lst, expand_n):
+		bank_expanded = []
+		for i in range(len(lst)):
+			#right_bank_expanded.append(right_bank_point)
+			bank_expanded.append(lst[i])
+			if i+1 < len(lst):
+				x_expand = np.linspace(lst[i][0], lst[i+1][0], expand_n)
+				y_expand = np.linspace(lst[i][1], lst[i+1][1], expand_n)
+				for j in range(len(x_expand)):
+					bank_expanded.append([x_expand[j],y_expand[j]])
+				bank_expanded.append(lst[i+1])
+		return bank_expanded
+	expanded = 10
+	right_bank_expanded = expand_list(right_bank_pairs, expanded)
+	left_bank_expanded =  expand_list(left_bank_pairs, expanded)
 
 	# Search for closest value in list of lists (KNN)
 	from scipy import spatial
+
 	# Closest neighbors to the right bank
+	plot_every_n = 100
 	right_bank_nearest_neighbor_x = []
 	right_bank_nearest_neighbor_y = [] # closest left pair
-	tree = spatial.KDTree(np.array(left_bank_pairs))
-	for right_pair in right_bank_pairs[::10]:
+	tree = spatial.KDTree(np.array(left_bank_expanded))
+	for right_pair in right_bank_pairs[::plot_every_n]:
 		distances, index = tree.query(right_pair)
 		closest_left_pair = list(tree.data[index])
 		right_bank_nearest_neighbor_x.append(right_pair[1])
 		right_bank_nearest_neighbor_x.append(closest_left_pair[1])
 		right_bank_nearest_neighbor_y.append(right_pair[0])
 		right_bank_nearest_neighbor_y.append(closest_left_pair[0])
+
 	#Closest neighbors to the left bank
 	left_bank_nearest_neighbor_x = []
 	left_bank_nearest_neighbor_y = [] # closest right pair
-	tree = spatial.KDTree(np.array(right_bank_pairs))
-	for left_pair in left_bank_pairs[::10]:
+	tree = spatial.KDTree(np.array(right_bank_expanded))
+	for left_pair in left_bank_pairs[::plot_every_n]:
 		distances, index = tree.query(left_pair)
 		closest_right_pair = list(tree.data[index])
 		left_bank_nearest_neighbor_x.append(left_pair[1])
@@ -108,4 +152,5 @@ if __name__ == "__main__":
 	plotRiver(df, latitude_points, longitude_points,
 			right_bank_nearest_neighbor_x, right_bank_nearest_neighbor_y,
 			left_bank_nearest_neighbor_x, left_bank_nearest_neighbor_y,
+			right_bank_expanded, left_bank_expanded,
 			 "data/river_coords.png")
