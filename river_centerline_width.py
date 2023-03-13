35e0471c092c7653323338ee7d796f03c05094a7
cyschneck <22159116+cyschneck@users.noreply.github.com>
2023-03-12 23:00:12 -0600

    pointsFromVoronoi(), centerlinePath() functions

--- river_centerline_width.py
+++ river_centerline_width.py
@@ -47,6 +47,16 @@ def convertColumnsToCSV(text_file=None, flipBankDirection=False):
 		write.writerow(header_fields)
 		write.writerows(total_rows)
 
+def leftRightCoordinates(dataframe):
+	right_bank_coordinates = [] # without nan
+	left_bank_coordinates = [] # wtihout nan
+	for index, row in df.iterrows():
+		if not math.isnan(row.rlat) and not math.isnan(row.rlon):
+			right_bank_coordinates.append([row.rlon, row.rlat])
+		if not math.isnan(row.llat) and not math.isnan(row.llon):
+			left_bank_coordinates.append([row.llon, row.llat])
+	return left_bank_coordinates, right_bank_coordinates
+
 ########################################################################
 def generatePolygon(left_bank_lst, right_bank_lst):
 	# Return a shapely polygon based on the position of the river bank points
@@ -72,6 +82,63 @@ def generateVoronoi(left_bank_lst, right_bank_lst):
 
 	river_voronoi = Voronoi(all_banks_points)
 	return river_voronoi
+
+def pointsFromVoronoi(river_voronoi, river_polygon):
+	# Returns a dictionary list of all the voronoi points: {start point : [list of end points]}
+	points_dict = {}
+	all_connections_start_to_end = []
+	for ridge_vertex_point in river_voronoi.ridge_vertices:
+		if ridge_vertex_point[0] >= 0 and ridge_vertex_point[1] >= 0: # Only include non-infinity vertex edges
+			v0 = river_voronoi.vertices[ridge_vertex_point[0]]
+			v1 = river_voronoi.vertices[ridge_vertex_point[1]]
+			# Check if start and end points are within the polygon, otherwise remove the connection pair
+			if river_polygon.contains(Point([v0[0], v0[1]])) and river_polygon.contains(Point([v1[0], v1[1]])):
+				start_point = tuple([v0[0], v0[1]])
+				end_point = tuple([v1[0], v1[1]])
+				start_to_end = [start_point, end_point]
+				if start_to_end not in all_connections_start_to_end: 
+					all_connections_start_to_end.append(start_to_end)
+
+	# Plot the connections that have at least one connection
+	connections_counter_start_to_end = Counter(x for xs in all_connections_start_to_end for x in set(xs)) # counter the amount of connections for each point
+	for connection in all_connections_start_to_end:
+		start_point = connection[0]
+		end_point = connection[1]
+		# Only plot points with at least two connections (removes any edges that are not connected to additional points)
+		if connections_counter_start_to_end[start_point] > 1:
+			if connections_counter_start_to_end[end_point] > 1: 
+				if start_point not in points_dict.keys():
+					points_dict[start_point] = []
+				points_dict[start_point].append(end_point)
+
+	return points_dict
+
+def centerlinePath(river_voronoi, river_polygon, top_polygon_line, bottom_polygon_line):
+	# Return the starting node, ending node, and centerline path
+	start_end_points_dict = pointsFromVoronoi(river_voronoi, river_polygon)
+	x_ridge_point = []
+	y_ridge_point = []
+	starting_node = None
+	ending_node = None
+	for start_point, end_point_list in start_end_points_dict.items():
+		if len(end_point_list) > 0: # TESTING TESTING: Show only the end points that have multiple connections
+			# Find the starting and ending node based on distance from the top and bottom of the polygon
+			if starting_node is None: starting_node = start_point
+			else:
+				if Point(start_point).distance(top_polygon_line) <= Point(starting_node).distance(top_polygon_line):
+					starting_node = start_point
+			for end_point in end_point_list:
+				if Point(end_point).distance(top_polygon_line) <= Point(starting_node).distance(top_polygon_line):
+					starting_node = end_point
+				if ending_node is None: ending_node = end_point
+				else:
+					if Point(start_point).distance(bottom_polygon_line) <= Point(ending_node).distance(bottom_polygon_line):
+						ending_node = start_point
+					if Point(end_point).distance(bottom_polygon_line) <= Point(ending_node).distance(bottom_polygon_line):
+						ending_node = end_point
+				x_ridge_point.append([start_point[0], end_point[0]])
+				y_ridge_point.append([start_point[1], end_point[1]])
+	return starting_node, ending_node, x_ridge_point, y_ridge_point, start_end_points_dict
 
 def networkXGraphShortestPath(all_points_dict, starting_node, ending_node):
 	def distanceBetween(start, end):
@@ -100,11 +167,11 @@ def networkXGraphShortestPath(all_points_dict, starting_node, ending_node):
 	return shortest_path
 ########################################################################
 
-def plotRiver(display_all_paths,
-			top_river_line, bottom_river_line,
-			right_bank_expanded, left_bank_expanded,
-			river_bank_polygon, river_bank_voronoi,
-			save_plot_name):
+def plotRiver(river_df=None, display_all_paths=False, save_plot_name=None):
+	# Plot river
+	left_bank_coordinates, right_bank_coordinates = leftRightCoordinates(river_df)
+	river_bank_polygon, top_river_line, bottom_river_line = generatePolygon(left_bank_coordinates, right_bank_coordinates)
+	river_bank_voronoi = generateVoronoi(left_bank_coordinates, right_bank_coordinates)
 
 	# Plot river based on right/left bank coordinates
 	fig = plt.figure(figsize=(10,10))
@@ -112,57 +179,8 @@ def plotRiver(display_all_paths,
 	
 	# Isolate center line:
 	# Plot the ridge edges of the Voronoi polygons that lie within the river banks
-	points_dict = {}
-	all_connections_start_to_end = []
-	for ridge_vertex_point in river_bank_voronoi.ridge_vertices:
-		if ridge_vertex_point[0] >= 0 and ridge_vertex_point[1] >= 0:
-			v0 = river_bank_voronoi.vertices[ridge_vertex_point[0]]
-			v1 = river_bank_voronoi.vertices[ridge_vertex_point[1]]
-			# Check if start and end points are within the polygon, otherwise remove the connection pair
-			if river_bank_polygon.contains(Point([v0[0], v0[1]])) and river_bank_polygon.contains(Point([v1[0], v1[1]])):
-				start_point = tuple([v0[0], v0[1]])
-				end_point = tuple([v1[0], v1[1]])
-				start_to_end = [start_point, end_point]
-				if start_to_end not in all_connections_start_to_end: 
-					all_connections_start_to_end.append(start_to_end)
-
-	connections_counter_start_to_end = Counter(x for xs in all_connections_start_to_end for x in set(xs))
-	# Plot the connections
-	for connection in all_connections_start_to_end:
-		start_point = connection[0]
-		end_point = connection[1]
-		# Only plot points with at least two connections
-		if connections_counter_start_to_end[start_point] > 1:
-			if connections_counter_start_to_end[end_point] > 1: 
-				#logger.debug("Start : {0} - {1}".format(start_point[0], connections_counter_start_to_end[start_point]))
-				#logger.debug("End   : {0} - {1}".format(end_point[0], connections_counter_start_to_end[end_point]))
-				if start_point not in points_dict.keys():
-					points_dict[start_point] = []
-				points_dict[start_point].append(end_point)
-
 	# Find the start and ending node to find the centerline
-	x_ridge_point = []
-	y_ridge_point = []
-	starting_node = None
-	ending_node = None
-	for start_point, end_point_list in points_dict.items():
-		if len(end_point_list) > 0: # TESTING TESTING
-			if starting_node is None: starting_node = start_point
-			else:
-				if Point(start_point).distance(top_river_line) <= Point(starting_node).distance(top_river_line):
-					starting_node = start_point
-			for end_point in end_point_list:
-				if Point(end_point).distance(top_river_line) <= Point(starting_node).distance(top_river_line):
-					starting_node = end_point
-				if ending_node is None: ending_node = end_point
-				else:
-					if Point(start_point).distance(bottom_river_line) <= Point(ending_node).distance(bottom_river_line):
-						ending_node = start_point
-					if Point(end_point).distance(bottom_river_line) <= Point(ending_node).distance(bottom_river_line):
-						ending_node = end_point
-				# Ridge Points for paths
-				x_ridge_point.append([start_point[0], end_point[0]])
-				y_ridge_point.append([start_point[1], end_point[1]])
+	starting_node, ending_node, x_ridge_point, y_ridge_point, start_end_points_dict = centerlinePath(river_bank_voronoi, river_bank_polygon, top_river_line, bottom_river_line)
 
 	# Plot River as a Polygon
 	plt.plot(*river_bank_polygon.exterior.xy, c="gainsboro")
@@ -187,25 +205,25 @@ def plotRiver(display_all_paths,
 	scatter_plot_size = 4
 	x = []
 	y = []
-	for i in right_bank_expanded: 
+	for i in right_bank_coordinates: 
 		x.append(i[0])
 		y.append(i[1])
 	plt.scatter(x, y, c="dodgerblue", s=scatter_plot_size, label="Right Bank")
 	x = []
 	y = []
-	for i in left_bank_expanded: 
+	for i in left_bank_coordinates: 
 		x.append(i[0])
 		y.append(i[1])
 	plt.scatter(x, y, c="orange", s=scatter_plot_size, label="Left Bank")
 
 	# Find centerline from NetworkX
-	shortest_path_points = networkXGraphShortestPath(points_dict, starting_node, ending_node)
+	shortest_path_points = networkXGraphShortestPath(start_end_points_dict, starting_node, ending_node)
 	valid_path_through = False
 	if shortest_path_points:
 		valid_path_through = True
 		plt.plot(*zip(*shortest_path_points), c="black", label="Centerline (Shortest Path)")
 
-	plt.title("River Coordinates: Valid Path = {0}, Valid Polygon = {1}".format(valid_path_through, river_bank_polygon.is_valid))
+	plt.title("River Coordinates: Valid Centerline = {0}, Valid Polygon = {1}".format(valid_path_through, river_bank_polygon.is_valid))
 	plt.xlabel("Longitude (°)")
 	plt.ylabel("Latitude (°)")
 	plt.legend(loc="upper right")
@@ -219,30 +237,17 @@ if __name__ == "__main__":
 	#df = df.head(10) # valid centerline, valid path, valid polygon, valid starting node, valid ending node
 	#df = df.head(100) # valid centerline, valid path, valid polygon, valid starting node, valid ending node
 	df = df.head(550) # valid centerline, valid path, valid polygon, valid starting node, valid ending node
+
 	#df = df.head(250) # valid centerline, valid path, invalid polygon, valid starting node, valid ending nodes
 	#df = df.head(40) # invalid centerline, valid path, valid polgyon, invalid starting node, valid ending node
 	#df = df.head(700) # invalid centerline, valid path, valid polgyon, invalid starting node, valid ending node
 	#df = df.head(1000) # invalid centerline, invalid path, invalid polgyon, invalid starting node, valid ending node
 
-	# Lines between points on graph
-	right_bank_coordinates = [] # without nan
-	left_bank_coordinates = [] # wtihout nan
-	for index, row in df.iterrows():
-		if not math.isnan(row.rlat) and not math.isnan(row.rlon):
-			right_bank_coordinates.append([row.rlon, row.rlat])
-		if not math.isnan(row.llat) and not math.isnan(row.llon):
-			left_bank_coordinates.append([row.llon, row.llat])
-
-	# Set up a polygon based on the left and right bank
-	polygon_river, top_river_line, bottom_river_line = generatePolygon(left_bank_coordinates, right_bank_coordinates)
-
-	# Set up Vornoi based on the left and right bank
-	voronoi_river = generateVoronoi(left_bank_coordinates, right_bank_coordinates)
-
 	# Plot river banks
-	display_all_paths = True # display all possible paths (not just centerline) (useful for debugging)
-	plotRiver(display_all_paths, 
-			top_river_line, bottom_river_line,
-			right_bank_coordinates, left_bank_coordinates,
-			polygon_river, voronoi_river,
-			 "data/river_coords.png")
+	display_debugging = True # display all possible paths (not just centerline) (useful for debugging)
+	plotRiver(river_df=df,
+			save_plot_name="data/river_coords.png",
+			display_all_paths=display_debugging)
+
+	# Return the latitude/longtiude coordinates for the centerline
+	# Return the width of the river for each centerline vertex (distance from right, left, total)
