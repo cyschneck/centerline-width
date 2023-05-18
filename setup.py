624a7fdd8a3669076d0d8366785021abdfdffa63
cyschneck <22159116+cyschneck@users.noreply.github.com>
2023-05-17 19:11:37 -0600

    v0.1.0 pre-release wtih existing functions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # Python Package Setup
 from setuptools import setup, find_namespace_packages
 
-VERSION="0.0.2"
+VERSION="0.1.0"
 DESCRIPTION="A Python package to find the centerline and width of rivers based on the latitude and longitude from a right and left bank"
 
 with open("README.md", "r") as f:
