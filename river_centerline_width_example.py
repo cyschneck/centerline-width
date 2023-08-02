5a18e3b00a9502af717f1f4e8cfc0ec6bb9c1c7e
cyschneck <22159116+cyschneck@users.noreply.github.com>
2023-08-02 15:46:28 -0600

    v1.2.0 coordinate_type -> coordinate_unit

--- river_centerline_width_example.py
+++ river_centerline_width_example.py
@@ -12,11 +12,11 @@ if __name__ == "__main__":
 
 	# Valid Examples
 	cutoff = None
-	#cutoff = 10
+	cutoff = 10
 	#cutoff = 15 # valid centerline, valid path, valid polygon, valid starting node, valid ending node
 	#cutoff = 30
 	#cutoff = 100 # valid centerline, valid path, valid polygon, valid starting node, valid ending node
-	cutoff = 550 # valid centerline, valid path, valid polygon, valid starting node, valid ending node
+	#cutoff = 550 # valid centerline, valid path, valid polygon, valid starting node, valid ending node
 	# Invalid Examples
 	#cutoff = 5 # invalid centerline, invalid path, valid polygon, invalid starting node, invalid ending nodes
 	#cutoff = 250 # valid centerline, valid path, invalid polygon, valid starting node, valid ending nodes
@@ -27,32 +27,34 @@ if __name__ == "__main__":
 	river = centerline_width.riverCenterline(csv_data="data/river_coords.csv",
 											optional_cutoff=cutoff,
 											interpolate_data=False,
-											interpolate_n=10,
-											interpolate_n_centerpoints=None,
-											equal_distance=20,
+											interpolate_n=200,
+											interpolate_n_centerpoints=200,
+											equal_distance=10,
 											ellipsoid="WGS84")
 
 	#print(river)
 	#print(river.__dict__.keys())
-	print("\nCenterline Length = {0} km".format(river.centerlineLength))
-	print("Centerline Length = {0} m".format(river.centerlineLength*1000))
-	print("Right Bank Length = {0} km".format(river.rightBankLength))
-	print("Left Bank Length = {0} km".format(river.leftBankLength))
+	#print("\nCenterline Length = {0} km".format(river.centerlineLength))
+	#print("Centerline Length = {0} m".format(river.centerlineLength*1000))
+	#print("Right Bank Length = {0} km".format(river.rightBankLength))
+	#print("Left Bank Length = {0} km".format(river.leftBankLength))
 	#print("ellipsoid = {0}".format(river.ellipsoid))
 	#print("centerlineVoronoi = {0}".format(river.centerlineVoronoi))
+	#print("centerlineVoronoiRelative = {0}".format(river.centerlineVoronoiRelative))
 	#print("equalDistanceCenterline = {0}".format(river.centerlineEqualDistance))
+	#print("equalDistanceCenterlineRelative = {0}".format(river.centerlineEqualDistanceRelative))
 	#print("centerlineEvenlySpaced = {0}".format(river.centerlineEvenlySpaced))
+	#print("centerlineEvenlySpacedRelative = {0}".format(river.centerlineEvenlySpacedRelative))
 	#print("centerlineSmoothed = {0}".format(river.centerlineSmoothed))
-	#print("centerlineVoronoi = {0}".format(len(river.centerlineVoronoi)))
-	#print("equalDistanceCenterline = {0}".format(len(river.centerlineEqualDistance)))
-	#print("centerlineEvenlySpaced = {0}".format(len(river.centerlineEvenlySpaced)))
-	#print("centerlineSmoothed = {0}".format(len(river.centerlineSmoothed)))
+	#print("centerlineSmoothedRelative = {0}".format(river.centerlineSmoothedRelative))
+	#print(river.left_bank_relative_coordinates)
 
-	coord_type = "reLATIVE DiSTANCE"
+	#coord_type = "relative DIStance"
+	coord_type = "Decimal Degrees"
 	center_type = "Voronoi"
 	
-	river.saveCenterlineCSV(save_to_csv="centerline_for_csv.csv", centerline_type=center_type, coordinate_type=coord_type)
-	river.saveCenterlineMAT(save_to_mat="centerline_for_matlab.mat", centerline_type=center_type, coordinate_type=coord_type)
+	river.saveCenterlineCSV(save_to_csv="centerline_for_csv.csv", centerline_type=center_type, coordinate_unit=coord_type)
+	river.saveCenterlineMAT(save_to_mat="centerline_for_matlab.mat", centerline_type=center_type, coordinate_unit=coord_type)
 	#river.saveCenterlineCSV(save_to_csv="centerline_for_csv.csv", latitude_header="lat", longitude_header="long", centerline_type="Equal Distance")
 	#river.saveCenterlineMAT(save_to_mat="centerline_for_matlab.mat", latitude_header="lat", longitude_header="long", centerline_type="Evenly Spaced")
 
@@ -61,27 +63,27 @@ if __name__ == "__main__":
 						centerline_type=center_type,
 						marker_type="scatter",
 						centerline_color="black",
-						display_all_possible_paths=True, 
+						display_all_possible_paths=False, 
 						display_voronoi=False,
 						plot_title=None,
-						coordinate_type=coord_type)
+						coordinate_unit=coord_type)
 	transect = 3
 
 	# Plot river bank width line
 	river.plotCenterlineWidth(save_plot_name=None, 
 							plot_title=None,
-							display_true_centerline=True,
+							display_true_centerline=False,
 							transect_span_distance=transect,
 							apply_smoothing=True,
 							flag_intersections=True,
-							remove_intersections=True,
-							coordinate_type=coord_type)
+							remove_intersections=False,
+							coordinate_unit=coord_type)
 
 	# Return width line for each centerline coordinates
 	river_width_dict = river.riverWidthFromCenterline(transect_span_distance=transect,
 													apply_smoothing=True,
-													remove_intersections=True,
-													save_to_csv="width_for_csv.csv",
-													coordinate_type=coord_type)
+													remove_intersections=False,
+													save_to_csv=None,
+													coordinate_unit=coord_type)
 
 	print("\nriver width dict = {0}\n".format(river_width_dict))
