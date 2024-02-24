a70ad1c0b65a4b35d61a0c2e96c02ebb0d68eb96
cyschneck <22159116+cyschneck@users.noreply.github.com>
2024-02-24 00:38:52 -0700

    python 3.12 support

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
 		"Programming Language :: Python :: 3.9",
 		"Programming Language :: Python :: 3.10",
 		"Programming Language :: Python :: 3.11",
+		"Programming Language :: Python :: 3.12",
 		"Intended Audience :: Education",
 		"Intended Audience :: Science/Research",
 		"Topic :: Scientific/Engineering :: Physics",
